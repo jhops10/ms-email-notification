d62cc61e57d75825329b7aeb53a1f551396b6eae
Joao Paulo <joaowais@gmail.com>
2025-09-30 16:00:16 -0300

    commit inicial

---
+++ src/main/java/com/jhops10/ms_email_notification/MsEmailNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.jhops10.ms_email_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEmailNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEmailNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhops10/ms_email_notification/MsEmailNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhops10.ms_email_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmailNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
